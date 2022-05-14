b43b2e2272239f7b2b13379ba01ee48d5f34b189
zezeze97 <18428308691@163.com>
2022-05-14 15:06:42 +0800

    test dubug

--- run.sh
+++ run.sh
@@ -12,6 +12,6 @@ if [ $1 = "train" ]; then
     CUDA_VISIBLE_DEVICES=$GPU PORT=23472 ./tools/dist_train.sh configs/convnext/${config}.py 1 --work-dir cache/${config} 
 elif [ $1 = "test" ]; then
     
-    CUDA_VISIBLE_DEVICES=$GPU python tools/test.py configs/convnext/${config}.py ./cache/${config}/latest.pth
+    CUDA_VISIBLE_DEVICES=$GPU python ./tools/test.py configs/convnext/${config}.py ./cache/${config}/latest.pth --show-dir output/${config}/
 fi
 
