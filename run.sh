d5be0edfcce8baabed9e3d7f77f2d18bb120e790
zezeze97 <18428308691@163.com>
2022-05-14 18:32:30 +0800

    fix

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# export PATH=/opt/conda/bin:$PATH
-# export PYTHONPATH=/home/zhangzr/mmsegmentation_kaggle:$PYTHONPATH
+export PATH=/opt/conda/bin:$PATH
+export PYTHONPATH=/home/zhangzr/mmsegmentation_kaggle:$PYTHONPATH
 GPU=$2
 
 config=upernet_convnext_base_fp16_512x512_160k_kaggle
@@ -9,7 +9,7 @@ config=upernet_convnext_base_fp16_512x512_160k_kaggle
 
 if [ $1 = "train" ]; then
    
-    CUDA_VISIBLE_DEVICES=$GPU PORT=23472 ./tools/dist_train.sh configs/convnext/${config}.py 2 --work-dir cache/${config} 
+    CUDA_VISIBLE_DEVICES=$GPU PORT=23472 ./tools/dist_train.sh configs/convnext/${config}.py 1 --work-dir cache/${config} 
 elif [ $1 = "test" ]; then
     
     CUDA_VISIBLE_DEVICES=$GPU python ./tools/test.py configs/convnext/${config}.py ./cache/${config}/latest.pth --show-dir output/${config}/
