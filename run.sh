d139dfd246884543a758c17398e97ac448cbcc21
zezeze97 <18428308691@163.com>
2022-05-14 21:19:15 +0800

    update

--- run.sh
+++ run.sh
@@ -12,6 +12,6 @@ if [ $1 = "train" ]; then
     CUDA_VISIBLE_DEVICES=$GPU PORT=23472 ./tools/dist_train.sh configs/convnext/${config}.py 1 --work-dir cache/${config} 
 elif [ $1 = "test" ]; then
     
-    CUDA_VISIBLE_DEVICES=$GPU python ./tools/test.py configs/convnext/${config}.py ./cache/${config}/iter_32000.pth --format-only --eval-options "imgfile_prefix=./test_results/upernet_convnext_base_fp16_512x512_160k_kaggle"
+    CUDA_VISIBLE_DEVICES=$GPU python ./tools/test.py configs/convnext/${config}.py ../../input/mmsegckpts/iter_32000.pth --format-only --eval-options "imgfile_prefix=./test_results/upernet_convnext_base_fp16_512x512_160k_kaggle"
 fi
 
