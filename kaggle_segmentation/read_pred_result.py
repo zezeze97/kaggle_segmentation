8b773299030cfeccb9d3368d0958a76ae3553f8d
zezeze97 <18428308691@163.com>
2022-05-14 20:45:15 +0800

    support test

---
+++ kaggle_segmentation/read_pred_result.py
@@ -0,0 +1,11 @@
+import cv2
+import os
+import numpy as np
+
+if __name__ == '__main__':
+    pred_result_root_path = 'test_results/upernet_convnext_base_fp16_512x512_160k_kaggle'
+    pred_img_name_list = os.listdir(pred_result_root_path)
+    for pred_img_name in pred_img_name_list:
+        pred_img = cv2.imread(os.path.join(pred_result_root_path, pred_img_name), cv2.IMREAD_UNCHANGED)
+        print(np.unique(pred_img))
+        print(pred_img.shape)
--- mmseg/datasets/custom.py
+++ mmseg/datasets/custom.py
@@ -1,4 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import os
+from PIL import Image
+import cv2
 import os.path as osp
 import warnings
 from collections import OrderedDict
@@ -248,8 +251,38 @@ class CustomDataset(Dataset):
         return self.pipeline(results)
 
     def format_results(self, results, imgfile_prefix, indices=None, **kwargs):
-        """Place holder to format result to dataset specific output."""
-        raise NotImplementedError
+        """Format the results into dir (standard format for Cityscapes
+        evaluation).
+        Args:
+            results (list): Testing results of the dataset.
+            imgfile_prefix (str | None): The prefix of images files. It
+                includes the file path and the prefix of filename, e.g.,
+                "a/b/prefix". If not specified, a temp file will be created.
+                Default: None.
+            to_label_id (bool): whether convert output to label_id for
+                submission. Default: False
+        Returns:
+            tuple: (result_files, tmp_dir), result_files is a list containing
+                the image paths, tmp_dir is the temporal directory created
+                for saving json/png files when img_prefix is not specified.
+        """
+
+        result_files = []
+        for res, idx in zip(results, indices):
+            if len(res.shape) == 3 and not np.issubdtype(res.dtype, np.integer):
+                result_file = osp.join(imgfile_prefix, self.img_infos[idx]["filename"][:-4] + ".png")
+                if not osp.exists(osp.dirname(result_file)):
+                    os.system(f"mkdir -p {osp.dirname(result_file)}")
+                cv2.imwrite(result_file, (res * 65535).astype(np.uint16))
+            else:
+                result_file = osp.join(imgfile_prefix, self.img_infos[idx]["filename"][:-4] + ".png")
+                if not osp.exists(osp.dirname(result_file)):
+                    os.system(f"mkdir -p {osp.dirname(result_file)}")
+                Image.fromarray(res.astype(np.uint8)).save(result_file)
+            result_files.append(result_file)
+
+        return result_files
+
 
     def get_gt_seg_map_by_idx(self, index):
         """Get one ground truth segmentation map for evaluation."""
