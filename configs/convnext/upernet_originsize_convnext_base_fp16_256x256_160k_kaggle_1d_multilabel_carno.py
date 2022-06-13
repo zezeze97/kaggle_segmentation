ae65ab29b76708eca493701bfb770c2740faf532
zezeze97 <18428308691@163.com>
2022-06-13 22:31:16 +0800

    update

---
+++ configs/convnext/upernet_originsize_convnext_base_fp16_256x256_160k_kaggle_1d_multilabel_carno.py
@@ -0,0 +1,145 @@
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+custom_imports = dict(imports='mmcls.models', allow_failed_imports=False)
+checkpoint_file = 'https://download.openmmlab.com/mmclassification/v0/convnext/downstream/convnext-base_3rdparty_32xb128-noema_in1k_20220301-2a0ee547.pth'
+model = dict(
+    type='Multi_Label_EncoderDecoder',
+    pretrained=None,
+    backbone=dict(
+        type='mmcls.ConvNeXt',
+        arch='base',
+        out_indices=[0, 1, 2, 3],
+        drop_path_rate=0.4,
+        layer_scale_init_value=1.0,
+        gap_before_final_norm=False,
+        init_cfg=dict(
+            type='Pretrained',
+            checkpoint=checkpoint_file,
+            prefix='backbone.')),
+    decode_head=dict(
+        type='UPerHeadOriginSize',
+        freeze_module=[], # freeze_module [str]: PSP, FPN
+        in_channels=[128, 256, 512, 1024],
+        in_index=[0, 1, 2, 3],
+        pool_scales=(1, 2, 3, 6),
+        channels=512,
+        dropout_ratio=0.1,
+        num_classes=3,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0)),
+    auxiliary_head=dict(
+        type='FCNHead',
+        in_channels=512,
+        in_index=2,
+        channels=256,
+        num_convs=1,
+        concat_input=False,
+        dropout_ratio=0.1,
+        num_classes=3,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=0.4)),
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole',multi_label=True))
+dataset_type = 'Kaggle_Dataset'
+data_root = 'data/mmseg_train_25d_carno/'
+classes = ['large_bowel', 'small_bowel', 'stomach']
+palette = [[64,64,64],[128,128,128],[255,255,255]]
+img_norm_cfg = dict(mean=[0,0,0], std=[1,1,1], to_rgb=True)
+crop_size = (256, 256)
+img_scale = (256, 256)
+train_pipeline = [
+    dict(type='LoadImageFromFile', to_float32=True, color_type='unchanged', force_uint8=True, force_3channel=True),
+    dict(type='LoadAnnotations',reduce_zero_label=False),
+    dict(type='Resize', img_scale=img_scale, keep_ratio=True),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='Normalize', **img_norm_cfg),
+    # dict(type='Pad', size=img_scale, pad_val=0, seg_pad_val=255),
+    dict(type='DefaultFormatBundle_Multilabel'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg'])
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile', to_float32=True, color_type='unchanged', force_uint8=True, force_3channel=True),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=img_scale,
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img'])
+        ])
+]
+data = dict(
+    samples_per_gpu=8,
+    workers_per_gpu=4,
+    train=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir='images_1d',
+        ann_dir='labels',
+        img_suffix=".png",
+        seg_map_suffix='.png',
+        split="splits/fold_0.txt",
+        classes=classes,
+        palette=palette,
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir='images_1d',
+        ann_dir='labels',
+        img_suffix=".png",
+        seg_map_suffix='.png',
+        split="splits/holdout_0.txt",
+        classes=classes,
+        palette=palette,
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        # data_root='../../input/uw-madison-gi-tract-image-segmentation/',
+        data_root=data_root,
+        test_mode=True,
+        # img_dir='test/images',
+        img_dir='images_1d',
+        img_suffix=".png",
+        seg_map_suffix='.png',
+        split="splits/holdout_0.txt",
+        classes=classes,
+        palette=palette,
+        pipeline=test_pipeline))
+
+log_config = dict(
+    interval=50, hooks=[dict(type='TextLoggerHook', by_epoch=False)])
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+cudnn_benchmark = True
+optimizer = dict(
+    constructor='LearningRateDecayOptimizerConstructor',
+    type='AdamW',
+    lr=0.0001,
+    betas=(0.9, 0.999),
+    weight_decay=0.05,
+    paramwise_cfg=dict(decay_rate=0.9, decay_type='stage_wise', num_layers=12))
+optimizer_config = dict(type='Fp16OptimizerHook', loss_scale='dynamic')
+lr_config = dict(
+    policy='poly',
+    warmup='linear',
+    warmup_iters=1500,
+    warmup_ratio=1e-06,
+    power=1.0,
+    min_lr=0.0,
+    by_epoch=False)
+runner = dict(type='IterBasedRunner', max_iters=160000)
+checkpoint_config = dict(by_epoch=False, interval=16000, max_keep_ckpts=1)
+evaluation = dict(interval=16000, metric='mDice', pre_eval=True, save_best='mDice')
+fp16 = dict()
+auto_resume = False
