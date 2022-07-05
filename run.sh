94f5dbdc4162aac34a079c06e3f5def972728683
zezeze97 <18428308691@163.com>
2022-07-05 16:37:07 +0800

    swin_v2

--- run.sh
+++ run.sh
@@ -12,10 +12,12 @@ GPU=$2
 # config=upernet_swin_base_patch4_window7_384x384_160k_kaggle25d_pretrain_224x224_22K_TTA_Dice
 # config=upernet_swin_base_patch4_window7_384x384_160k_kaggle25d_pretrain_224x224_22K_TTA_class_weight
 # config=upernet_swin_base_patch4_window7_384x384_160k_kaggle25d_pretrain_224x224_22K_TTA_OHEM
-config=upernet_swin_base_patch4_window7_384x384_160k+160k_kaggle25d_pretrain_224x224_22K_TTA_OHEM
+# config=upernet_swin_base_patch4_window7_384x384_160k+160k_kaggle25d_pretrain_224x224_22K_TTA_OHEM
+config=upernet_swinv2_base_160k_pretrained_patch4_window12_192_22k_OHEM_TTA
 if [ $1 = "train" ]; then
     # CUDA_VISIBLE_DEVICES=$GPU PORT=23471 ./tools/dist_train.sh configs/convnext/${config}.py 2 --work-dir cache/${config} 
-    CUDA_VISIBLE_DEVICES=$GPU PORT=23472 ./tools/dist_train.sh configs/swin/${config}.py 2 --work-dir cache/${config} 
+    # CUDA_VISIBLE_DEVICES=$GPU PORT=23472 ./tools/dist_train.sh configs/swin/${config}.py 2 --work-dir cache/${config} 
+    CUDA_VISIBLE_DEVICES=$GPU PORT=23472 ./tools/dist_train.sh configs/swin_v2/${config}.py 2 --work-dir cache/${config} 
 elif [ $1 = "test" ]; then
     
     # CUDA_VISIBLE_DEVICES=$GPU python ./tools/test.py configs/convnext/${config}.py ../../input/mmsegckpts/iter_1600.pth --format-only --eval-options "imgfile_prefix=./test_results/upernet_convnext_base_fp16_256x256_16k_kaggle_no_crop"
